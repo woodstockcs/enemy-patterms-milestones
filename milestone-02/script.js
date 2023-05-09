a137cd6305d0c0f352a15289a3eeda21eb11545d
andytilia <andytilia@users.noreply.github.com>
2023-05-09 12:49:04 -0400

    Update script.js

--- milestone-02/script.js
+++ milestone-02/script.js
@@ -2,7 +2,7 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 300;
-CANVAS_HEIGTH = canvas.heigth= 500;
+CANVAS_HEIGHT = canvas.height= 500;
 
 enemy1={
   x: 0,
@@ -12,6 +12,6 @@ enemy1={
 }
 
 function animate(){
-  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.heigth);
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
 }
 animate();
