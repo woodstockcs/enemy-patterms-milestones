b77a95411f682a790d57b059ab64785bb828bfe6
EdwinL06 <133036762+EdwinL06@users.noreply.github.com>
2023-05-09 11:37:33 -0500

    Update script.js

--- milestone-03/script.js
+++ milestone-03/script.js
@@ -1,1 +1,21 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 300);
+const CANVAS_HEIGHT = (canvas.height = 500);
+
+const enemy1 = {
+  x: 10,
+  y: 50,
+  width: 100,
+  height: 100,
+};
+
+function animate() {
+  ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+  enemy1.x++;
+  enemy1.y++;
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+  requestAnimationFrame(animate);
+}
+animate();
 
